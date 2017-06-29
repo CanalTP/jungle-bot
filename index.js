63d2bb2e75f6e204dcded6c304e71fa7c0d7d73d
Thomas Noury <thomas.noury@kisio.org>
2017-06-29 17:12:56 +0200

    Fixed infinite loop

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import Rx from 'rxjs/Rx';
 import R from 'ramda';
-import BroidSlack from '@broid/slack';
+import BroidSlack from 'broid-slack';
 import MessageBuilder from './MessageBuilder';
 
 const clients = {
     slack: new BroidSlack({
-        token: 'xoxb-204765474528-urhymN45whbjKfUeW9Osa1yi',
+        token: 'xoxb-204765474528-bzImpkDGf62N4YVUjLMuLZzk',
         http: {
             host: '127.0.0.1',
             port: 8080,
@@ -21,10 +21,11 @@ Rx.Observable.merge(...R.map(client => client.connect(), R.values(clients))).sub
 Rx.Observable.merge(...R.map(client => client.listen(), R.values(clients))).subscribe({
     next: message => {
         const messageType = R.path(['object', 'type'], message);
+        const senderType = R.path(['actor', 'type'], message);
         const generatorName = R.path(['generator', 'name'], message);
-        if (messageType === 'Note') {
+        if (messageType === 'Note' && senderType === 'Person') {
             const reply = MessageBuilder.getReply("Ma réponse est 42", message);
-            return clients[generatorName].send(reply)
+            clients[generatorName].send(reply)
                 .then(console.log)
                 .catch(console.error);
         }
