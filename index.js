454c1e57bf809a2442d7b1b66719058fcfa5903d
Jean-Baptiste Crestot <jbcrestot@gmail.com>
2017-06-30 16:44:10 +0200

    Refacto Router (#15)
    * refacto continue
    
    * fixed rebase
    
    * ajout multi routes
    
    * fin de merge

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Rx from 'rxjs/Rx';
 import R from 'ramda';
 import BroidSlack from 'broid-slack';
-import Routes from './Routes';
+import { Router } from './lib/router/index.js';
 import {clients} from './lib/clients';
 import {token} from './lib/token';
 
@@ -24,7 +24,7 @@ Rx.Observable.merge(...R.map(client => client.listen(), R.values(clients))).subs
         const senderType = R.path(['actor', 'type'], message);
 
         if (messageType === 'Note' && senderType === 'Person') {
-            R.map(route => route.execute(message), Routes);
+          Router(message);
         }
     },
     error: err => console.error(`Something went wrong: ${err.message}`),
