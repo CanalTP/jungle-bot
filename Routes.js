9774ae763329af621fee19f8d955120c05c6f493
Thomas Noury <thomas.noury@kisio.org>
2017-06-30 15:33:39 +0200

    Isochrons will dominates the world!

--- Routes.js
+++ Routes.js
@@ -5,14 +5,21 @@ export default {
     lastDeparture: {
         execute: (message) => {
             Router('Dernier départ vers (.*)', message.object.content, (destination) => {
-                Features.lastDeparture(message, destination)
+                Features.lastDeparture(message, destination);
             });
         }
     },
     nextDepartures: {
         execute: (message) => {
             Router('prochain(?:s)? (.*)? (?:de|à|pour) (.*)', message.object.content, (type, origin) => {
-                Features.nextDepartures(message, type, origin)
+                Features.nextDepartures(message, type, origin);
+            });
+        }
+    },
+    isochron: {
+        execute: (message) => {
+            Router('temps de trajet à partir de (.*) dans un rayon de ([0-9]+) minutes', message.object.content, (origin, duration) => {
+                Features.isochron(message, origin, duration);
             });
         }
     }
