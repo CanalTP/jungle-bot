55f133d07ce8acc893ed27703df48c96f4a8cfd5
Johan Rouve <johan.rouve@gmail.com>
2017-06-30 11:31:44 +0200

    add next departures

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import Router from './router/router';
 
 const clients = {
     slack: new BroidSlack({
-        token: 'xoxb-204765474528-HDKk3k8Sc1e7287XsKQPccjr',
+        token: 'xoxb-204765474528-UMGOH29DvI8enVN3xb1XdjfV',
         http: {
             host: '127.0.0.1',
             port: 8080,
@@ -40,6 +40,54 @@ Rx.Observable.merge(...R.map(client => client.listen(), R.values(clients))).subs
 
         if (messageType === 'Note' && senderType === 'Person') {
             const navitia = new Navitia('5c60fc34-e017-4c9e-9744-13515c6436d8');
+
+            Router('prochain(?:s)? (.*)? (?:de|à|pour) (.*)', messageBody, (type, origin) => {
+                console.log("type", type);
+                console.log("origin", origin);
+
+                if (type.match(new RegExp('train(s)?|rer', 'gi'))) {
+                    physical_modes.push('physical_mode:Train', 'physical_mode:RapidTransit');
+                }
+                if (type.match(new RegExp('métro(s)?', 'gi'))) {
+                    physical_modes.push('physical_mode:Metro');
+                }
+                if (type.match(new RegExp('bus', 'gi'))) {
+                    physical_modes.push('physical_mode:Bus');
+                }
+
+                navitia.getFirstPlace(origin, ["stop_area"])
+                    .then(place => {
+                        navitia.getDepartures(place.id)
+                            .then(departuresResponse => {
+                                let replyMessage = `Prochains passages à ${place.name}\n`;
+                                departuresResponse.departures.forEach(departure => {
+                                    let physical_mode;
+                                    departure.links.forEach(link => {
+                                        if (link.type == 'physical_mode') {
+                                            physical_mode = link.id;
+                                        }
+                                    });
+                                    replyMessage += `${formatDatetime(departure.stop_date_time.departure_date_time)} - ${getPicto(physical_mode)} \`${departure.display_informations.code}\` en direction de ${departure.display_informations.direction}\n`;
+                                });
+                                const reply = MessageBuilder.getReply(replyMessage, message);
+                                clients[generatorName].send(reply)
+                                    .then(console.log)
+                                    .catch(console.error);
+                            })
+                            .catch(result => {
+                                const reply = MessageBuilder.getReply(`Aucun horaire trouvé pour l'arrêt ${place.name}`, message);
+                                clients[generatorName].send(reply)
+                                    .then(console.log)
+                                    .catch(console.error);
+                            });
+                    })
+                    .catch(result => {
+                        const reply = MessageBuilder.getReply(`Désolé je connais pas ${origin}`, message);
+                        clients[generatorName].send(reply)
+                            .then(console.log)
+                            .catch(console.error);
+                    });
+            });
 
             Router('Dernier départ vers (.*)', messageBody, (destination) => {
                 navitia.getFirstPlace(destination)
@@ -74,3 +122,23 @@ Rx.Observable.merge(...R.map(client => client.listen(), R.values(clients))).subs
     },
     error: err => console.error(`Something went wrong: ${err.message}`),
 });
+
+function getPicto(physical_mode) {
+    switch (physical_mode) {
+        case 'physical_mode:Bus':
+            return ':bus:';
+        case 'physical_mode:Metro':
+            return ':metro:';
+        case 'physical_mode:Train':
+            return ':steam_locomotive:';
+        case 'physical_mode:RapidTransit':
+            return ':light_rail:';
+        case 'physical_mode:Tramway':
+            return ':tram:';
+        case 'physical_mode:Car':
+            return ':car:';
+        case 'physical_mode:Bike':
+        case 'physical_mode:BikeSharingService':
+            return ':bike:';
+    }
+}
