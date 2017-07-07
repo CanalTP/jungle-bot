6a463146ef8a6928d0353055c80e6ac8190c21ea
Johan Rouve <johan.rouve@gmail.com>
2017-07-07 10:05:07 +0200

    send question to wit for improve learning

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Rx from 'rxjs/Rx';
 import R from 'ramda';
-import BroidSlack from 'broid-slack';
-import { Router } from './lib/router/index.js';
+import BroidSlack from '@broid/slack';
+import {Wit} from 'node-wit';
+import {Router} from './lib/router/index.js';
 import {clients} from './lib/clients';
 
 clients.slack = new BroidSlack({
@@ -23,8 +24,20 @@ Rx.Observable.merge(...R.map(client => client.listen(), R.values(clients))).subs
         const senderType = R.path(['actor', 'type'], message);
 
         if (messageType === 'Note' && senderType === 'Person') {
-          Router(message);
+            Router(message);
+            postMessageOnWit(message.object.content);
         }
     },
     error: err => console.error(`Something went wrong: ${err.message}`),
 });
+
+function postMessageOnWit(message) {
+    if (process.env.WIT_TOKEN) {
+        const client = new Wit({accessToken: process.env.WIT_TOKEN});
+        client.message(message, {})
+            .then((data) => {
+                console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+            })
+            .catch(console.error);
+    }
+}
